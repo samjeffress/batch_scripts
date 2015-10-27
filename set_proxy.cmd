16ac078263b589d4d291a6bca30679a00a75b979
Sam Jeffress <s.jeffress@nib.com.au>
2015-10-27 12:32:17 +1100

    setting / unsetting proxy for cntlm

---
+++ set_proxy.cmd
@@ -0,0 +1,4 @@
+set https_proxy=http://localhost:3128
+set http_proxy=http://localhost:3128
+set HTTPS_PROXY=http://localhost:3128
+set HTTP_PROXY=http://localhost:3128
---
+++ unset_proxy.cmd
@@ -0,0 +1,4 @@
+set https_proxy=
+set http_proxy=
+set HTTPS_PROXY=
+set HTTP_PROXY=
